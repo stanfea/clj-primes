ffededeb1819a248e7eff68be2f3329bc0233c94
Stefan Bacon <stefan.bacon@gmail.com>
2016-07-21 00:15:50 +0200

    save

--- src/primes/core.clj
+++ src/primes/core.clj
@@ -2,18 +2,25 @@
   (:require [clojure.string :as string])
   (:gen-class))
 
-(defn gen-primes []
+(defn multiple?
+  "check if n is a multiple of m"
+  [n m]
+  (zero? (rem n m)))
+
+(defn gen-primes
   "generates a lazy sequence of prime numbers"
+  []
   ((fn fun [n primes]
-     (if (not-any? (fn [prime] (zero? (rem n prime)))
-                      (filter #(<= % (Math/sqrt n)) primes))
+     (if (not-any? (partial multiple? n)
+                      (take-while #(<= % (Math/sqrt n)) primes))
        (lazy-seq (cons n (fun (inc n) (conj primes n))))
        (recur (inc n) primes)))
    2 []))
 
 
-(defn gen-mul-table [coll]
+(defn gen-mul-table
   "generates a multiplication table"
+  [coll]
   (->> coll
        (map (fn [x] (cons x (map #(* x %) coll))))
        (cons (cons \* coll))))
@@ -23,8 +30,9 @@
       (println (string/join "\t" row))))
 
 
-(defn -main []
+(defn -main
   "prints a multiplication table of the first 10 primes"
+  []
   (->> (gen-primes)
        (take 10)
        gen-mul-table
